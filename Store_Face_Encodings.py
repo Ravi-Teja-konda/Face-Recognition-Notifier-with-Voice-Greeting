dd3058a8aa2ec159de05e5bf2a3358041072ba53
Ravi Teja <39398773+Ravi-Teja-konda@users.noreply.github.com>
2020-07-11 00:36:31 +0530

    Create Store_Face_Encodings.py

---
+++ Store_Face_Encodings.py
@@ -0,0 +1,26 @@
+import face_recognition
+import pickle
+import os
+from tqdm import tqdm
+
+#######################################################################################################################################
+
+all_face_encodings = {}
+
+
+print("Loading know face encodings... \nPlease wait..It may take some momemts....")
+
+
+images = os.listdir('images')
+
+for image in tqdm(images,ncols=100):
+    # load the image
+    #print(image)
+    known_image = face_recognition.load_image_file("images/" + image)
+
+    # Get the face encodings for the known images
+    #time.sleep(1)
+    all_face_encodings[str(image[:-4])] = face_recognition.face_encodings(known_image)[0]
+
+with open('dataset_faces.dat', 'wb') as f:
+    pickle.dump(all_face_encodings, f)
