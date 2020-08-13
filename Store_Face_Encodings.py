31159c5448f84f6ffd0a2e692e61a777b8c82ba3
Ravi Teja <39398773+Ravi-Teja-konda@users.noreply.github.com>
2020-08-13 23:36:13 +0530

    Update Store_Face_Encodings.py

--- Store_Face_Encodings.py
+++ Store_Face_Encodings.py
@@ -15,11 +15,9 @@ images = os.listdir('images')
 
 for image in tqdm(images,ncols=100):
     # load the image
-    #print(image)
     known_image = face_recognition.load_image_file("images/" + image)
 
     # Get the face encodings for the known images
-    #time.sleep(1)
     all_face_encodings[str(image[:-4])] = face_recognition.face_encodings(known_image)[0]
 
 with open('dataset_faces.dat', 'wb') as f:
